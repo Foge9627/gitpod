514c93668109347b0007a179a4d1826aee8bb13b
Laurie T. Malau <laurie@gitpod.io>
2022-04-28 11:10:04 +0000

    try log test

--- components/public-api-server/middleware/logging_test.go
+++ components/public-api-server/middleware/logging_test.go
@@ -1,13 +1,18 @@
 package middleware
 
 import (
+	"bytes"
+	log "github.com/sirupsen/logrus"
 	_ "github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestLoggingMiddleware(t *testing.T) {
+	logInMemory := &bytes.Buffer{}
+	log.SetOutput(logInMemory)
 	expectedBody := `hello world`
 
 	someHandler := http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
@@ -27,4 +32,6 @@ func TestLoggingMiddleware(t *testing.T) {
 	if rec.Body.String() != expectedBody {
 		t.Errorf("Unexpected body: %v", rec.Body.String())
 	}
+
+	assert.Equal(t, logInMemory, logInMemory)
 }
